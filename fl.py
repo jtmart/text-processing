d0850afaba01b6731b2d5c27651096bd482fa7a6
Jean-Thomas Martelli <jeanthomas.martelli@sciencespo.fr>
2020-09-24 08:16:30 +0530

    s

---
+++ fl.py
@@ -0,0 +1,17 @@
+import os.path
+directory=r'C:\Users\jtmartelli\Desktop\unionstatements\test2' #where txt are, python file can be outside of target directory
+for i in (os.scandir(directory)):
+	if (i=="fl.py"): #change python script name if filename.py changes
+		continue
+	#n=os.path.join(directory,i)
+	#for j in os.scandir(n):
+	if (i.path.endswith(".txt")):
+		file=open(i,encoding="utf-8") #alt encoding if necessary
+		s=file.readline()
+		if (s!="" or s!='\n'):
+			print (s.replace('\n',""))
+		else:
+			print ("not found"+str(j))
+
+
+
---
+++ workflow/removeBonusScrapfirstlineandputascsvdetail.py
@@ -0,0 +1,17 @@
+import os.path
+directory=r'C:\Users\anand_zas2udg\Documents\RBI Speeches'
+for i in (os.scandir(directory)):
+	if (i=="BonusScrapfirstlineandputascsvdetail.py"):
+		continue
+	n=os.path.join(directory,i)
+	for j in os.scandir(n):
+		if (j.path.endswith(".txt")):
+			file=open(j,encoding="utf-8")
+			s=file.readline()
+			if (s!="" or s!='\n'):
+				print (s.replace('\n',""))
+			else:
+				print ("not found"+str(j))
+
+
+
